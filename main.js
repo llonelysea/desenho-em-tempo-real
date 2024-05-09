ea5fdd99d2bf337a95dd1a569a084816332fe605
kyo <114775706+llonelysea@users.noreply.github.com>
2024-05-09 20:35:51 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
+noseX=0;
+noseY=0;
+difference= 0;
+rightWristX= 0;
+leftWristX= 0;
+
+
 function setup(){
     video=createCapture(VIDEO);
     video.size(550,500);
 
-    canvas = createCanvas(550,550);
-    canvas.position(560,150);
+    canvas = createCanvas(450,450);
+    canvas.position(560,120);
 
     poseNet = ml5.poseNet(video, modelLoaded);
     poseNet.on('pose', gotPoses);
@@ -15,11 +22,24 @@ function modelLoaded(){
 
 function draw(){
     background('#969A97');
+
+    document.getElementById("square_side").innerHTML = "largura e altura serão ="+difference+"px";
+    fill('#F90093');
+    stroke('#F90093');
+    square(noseX, noseY, difference);
 }
 
 function gotPoses(results){
     if(results.length > 0){
         console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX ="+ noseX +"noseY ="+ noseY);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+        console.log("leftWristX ="+leftWristX+"rightWristX ="+rightWristX+"difference ="+difference);
     }
 }
 
